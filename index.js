aa4da96cb7c9de29b38d41e3bb74c607f27c74c6
Javier Provecho Fernández <javiertitan@gmail.com>
2015-08-22 19:27:50 +0000

    Setup Heroku's env var

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 var express = require('express');
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
+var server = app.listen(app.get('port'), function () {
+  console.log('Node app is running on port', app.get('port'));
 });
