9f4da31e6b402e8fa4eed857c6883422ee809c19
Javier Provecho Fernández <javiertitan@gmail.com>
2015-08-22 23:53:10 +0000

    Finish API design

--- index.js
+++ index.js
@@ -1,14 +1,72 @@
+//////////////////////////////////////////////////////////////////
+// REQUIREs
+//////////////////////////////////////////////////////////////////
 var express = require("express");
+var Waterline = require('waterline');
+var passport = require('passport');
+var EveOnlineStrategy = require('passport-eveonline');
+var postgres = require('sails-postgresql');
 
 var app = express();
+var orm = new Waterline();
 
-var passport = require('passport');
+//////////////////////////////////////////////////////////////////
+// ENV VARs
+//////////////////////////////////////////////////////////////////
 
 app.set('secret', process.env.SECRET);
+app.set('EVEONLINE_CLIENT_ID', process.env.EVEONLINE_CLIENT_ID);
+app.set('EVEONLINE_SECRET_KEY', process.env.EVEONLINE_SECRET_KEY);
+app.set('EVEONLINE_AUTH_HOST', process.env.EVEONLINE_AUTH_HOST);
+app.set('HOST', process.env.HOST);
+app.set('DATABASE_URL', process.env.DATABASE_URL);
+app.set('port', (process.env.PORT || 5000));
+
+//////////////////////////////////////////////////////////////////
+// WATERLINE SETUP
+//////////////////////////////////////////////////////////////////
+
+var config = {
+    adapters: {
+        'default': postgres,
+        postgres: postgres
+    },
+    connections: {
+        herokuPostgres: {
+            adapter: 'postgres',
+            url: app.get('DATABASE_URL'),
+            pool: false,
+            ssl: true
+        }
+    },
+    defaults: {
+        migrate: 'alter'
+    }
+};
+
+//////////////////////////////////////////////////////////////////
+// WATERLINE MODELs
+//////////////////////////////////////////////////////////////////
+
+var Routes = Waterline.Collection.extend({
+
+  identity: 'routes',
+  connection: 'herokuPostgres',
+
+  attributes: {
+    origin: 'string',
+    destination: 'string',
+    price: 'integer'
+  }
+});
+
+//////////////////////////////////////////////////////////////////
+// EXPRESS SETUP
+//////////////////////////////////////////////////////////////////
 
 app.use(require('serve-static')(__dirname + '/public'));
 app.use(require('cookie-parser')());
-app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('body-parser').json());
 app.use(require('express-session')(
     {
         secret: app.get('secret'),
@@ -20,12 +78,9 @@ app.use(require('express-session')(
 app.use(passport.initialize());
 app.use(passport.session());
 
-var EveOnlineStrategy = require('passport-eveonline')
-
-app.set('EVEONLINE_CLIENT_ID', process.env.EVEONLINE_CLIENT_ID);
-app.set('EVEONLINE_SECRET_KEY', process.env.EVEONLINE_SECRET_KEY);
-app.set('EVEONLINE_AUTH_HOST', process.env.EVEONLINE_AUTH_HOST);
-app.set('HOST', process.env.HOST);
+//////////////////////////////////////////////////////////////////
+// PASSPORT SETUP
+//////////////////////////////////////////////////////////////////
 
 passport.use(new EveOnlineStrategy({
         clientID: app.get('EVEONLINE_CLIENT_ID'),
@@ -47,55 +102,61 @@ passport.serializeUser(function(name, done) {
 });
 
 passport.deserializeUser(function(name, done) {
-    done(null, name)
+    done(null, name);
 });
 
-var Waterline = require('waterline');
-var orm = new Waterline();
+//////////////////////////////////////////////////////////////////
+// EXPRESS API ROUTING
+//////////////////////////////////////////////////////////////////
 
-app.set('DATABASE_URL', process.env.DATABASE_URL);
-var postgres = require('sails-postgresql');
-var config = {
-    adapters: {
-        'default': postgres,
-        postgres: postgres
-    },
-    connections: {
-        herokuPostgres: {
-            adapter: 'postgres',
-            url: app.get('DATABASE_URL'),
-            pool: false,
-            ssl: true
-        }
-    },
-    defaults: {
-        migrate: 'alter'
-    }
-};
-
-var Routes = Waterline.Collection.extend({
-
-  identity: 'routes',
-  connection: 'herokuPostgres',
-
-  attributes: {
-    origin: {
-        type: 'string',
-        required: true
-    },
-    destination: {
-        type: 'string',
-        required: true,
-    },
-    price: {
-        type: 'integer',
-        required: true,
-        min: 0
-    }
-  }
+app.get('/routes/all', function(req, res) {
+    app.models.routes.find().exec(function(err, models) {
+        if(err) return res.status(500).json({ err: err });
+        res.json(models);
+    });
 });
 
-orm.loadCollection(Routes);
+app.post('/routes/search', function(req, res) {
+    app.models.routes
+        .find()
+        .where(req.body)
+        .exec(
+            function(err, model) {
+                if(err) return res.status(500).json({ err: err });
+                res.json(model);
+        });
+});
+
+app.post('/routes/create', function(req, res) {
+    app.models.routes
+        .create(req.body,
+        function(err, model) {
+            if(err) return res.status(500).json({ err: err });
+            res.json(model);
+        });
+});
+
+app.post('/routes/delete', function(req, res) {
+    app.models.routes
+        .destroy(req.body,
+            function(err) {
+                if(err) return res.status(500).json({ err: err });
+                res.json({ status: 'ok' });
+            });
+});
+
+app.get('/auth/status', function(req, res){
+    if(req.user) {
+        //res.status(200).send({ message: 'Keep moving! ;)' });
+        res.Next();
+    } else {
+        res.status(401).send({ message: 'None shall pass!' });
+    }
+});
+
+//////////////////////////////////////////////////////////////////
+// EXPRESS WEB ROUTING
+//////////////////////////////////////////////////////////////////
 
 app.get('/', function (req, res) {
     res.send('Welcome foreigner!');
@@ -109,14 +170,9 @@ app.get('/error', function (req, res) {
     res.send('We couldn\'t authenticate you, sorry.');
 });
 
-app.get('/auth/status', function(req, res){
-    if(req.user) {
-        //res.status(200).send({ message: 'Keep moving! ;)' });
-        res.Next();
-    } else {
-        res.status(401).send({ message: 'None shall pass!' });
-    }
-});
+//////////////////////////////////////////////////////////////////
+// EXPRESS AUTH ROUTING
+//////////////////////////////////////////////////////////////////
 
 app.get('/auth/eveonline',
     passport.authenticate('eveonline')
@@ -129,15 +185,18 @@ app.get('/auth/eveonline/callback',
     })
 );
 
+//////////////////////////////////////////////////////////////////
+// STARTUP
+//////////////////////////////////////////////////////////////////
+
+orm.loadCollection(Routes);
 orm.initialize(config, function(err, models) {
     if(err) throw err;
 
     app.models = models.collections;
     app.connections = models.connections;
     
-    app.set('port', (process.env.PORT || 5000));
-    
-    var server = app.listen(app.get('port'), function () {
+    app.listen(app.get('port'), function () {
         console.log('Node app is running on port', app.get('port'));
     });
 });
