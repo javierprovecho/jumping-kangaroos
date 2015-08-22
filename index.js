ee444561fcf3147f0af4277b5145b03bfc1f5518
Javier Provecho Fernández <javiertitan@gmail.com>
2015-08-22 19:35:14 +0000

    Add Passport

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-var express = require('express');
+var express = require("express");
+var passport = require('passport');
 var app = express();
+
+app.set('secret', (process.env.SECRET || 'keyboard cat'));
+
+app.use(require('serve-static')(__dirname + '/public'));
+app.use(require('cookie-parser')());
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('express-session')({ secret: app.get('secret'), resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.set('port', (process.env.PORT || 5000));
 
